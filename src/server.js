64fcfb6abf0252a7436eee8dfa8ec6065a166ab5
SnehDec-TheResurrection <snehasridhar2001@gmail.com>
2025-07-23 22:19:24 -0400

    Create server.js

---
+++ src/server.js
@@ -0,0 +1,41 @@
+import express from 'express';
+import { createServer } from 'http';
+import connectDB from './config/db.js';
+import config from './config/index.js';
+import authRoutes from './core/routes/authRoutes.js';
+import sensorDataRoutes from './core/routes/sensorDataRoutes.js';
+
+const app = express();
+const server = createServer(app);
+
+app.use(express.json());
+#connectDB();
+
+app.use('/api/auth', authRoutes);
+app.use('/api/sensor-data', sensorDataRoutes);
+app.use(express.text());
+
+server.listen(config.port, () => {
+  console.log(`Server running on port ${config.port}`);
+});
+
+server.on('error', (err) => {
+  if (err.code === 'EADDRINUSE') {
+    console.error(`Port ${config.port} is already in use. Terminate its running process or use a different port.`);
+  } else {
+    console.error('Server startup error:', err);
+  }
+});
+
+app.post("/esp32", function(req, res) {
+  const csv_data = req.body;
+
+  fs.appendFile(__dirname + '/esp32_comms.txt', csv_data + '\n', err => {
+    if (err) {
+      console.log(err);
+      res.send("Could not write to file");
+    } else {
+      res.send("Successfully wrote to file");
+    }
+  });
+});
