ff83da0ed78afaed5cc19eea641da8550a0965f2
SnehDec-TheResurrection <snehasridhar2001@gmail.com>
2025-07-24 02:25:54 -0400

    Update db.js

--- src/config/db.js
+++ src/config/db.js
@@ -1,1 +1,17 @@
+import mongoose from 'mongoose';
+import config from './index.js';
 
+const connectDB = async () => {
+  try {
+    await mongoose.connect(process.env.MONGO_URL {
+      useNewUrlParser: true,
+      useUnifiedTopology: true
+    });
+    console.log('MongoDB connected');
+  } catch (err) {
+    console.error('DB connection failed:', err.message);
+    process.exit(1);
+  }
+};
+
+export default connectDB;
