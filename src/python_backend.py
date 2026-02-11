c715f7da75f86e2ba4cf1fbed38c3d74556c420e
SnehDec-TheResurrection <snehasridhar2001@gmail.com>
2026-02-10 22:33:38 -0500

    Update python_backend.py

--- src/python_backend.py
+++ src/python_backend.py
@@ -1,9 +1,6 @@
 import sys
 
-def read_csv_test(sent_string):
 # read CSV row from stdin
-  csv_row = sent_string
-  return csv_row
-
-read_csv_test(sys.stdin.read().strip())
+csv_row = sys.stdin.read().strip()
+print(csv_row)
 
