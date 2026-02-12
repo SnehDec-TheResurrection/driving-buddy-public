5b003be26360aef4b2a4617279b8ac1acde62a9a
SnehDec-TheResurrection <snehasridhar2001@gmail.com>
2026-02-12 12:16:01 -0500

    Add code to pull elements from collection into python queue.

--- src/python_backend.py
+++ src/python_backend.py
@@ -15,10 +15,23 @@ def enqueue(queue, item):
 def dequeue(queue):
   queue.pop(0)
 
+def fetch_items(collection):
+  last_thirty_items = list(
+    collection.find()
+    .sort("timestamp", -1)  # newest first
+    .limit(30))
+  return last_thirty_items
+
+def fetch_item(collection): 
+  last_item_singular = collection.find_one(sort=[("timestamp", -1)])
+  return last_item_singular
+  
 sensorData = connect_to_DB()
-
-queue_of_events = [] # the size of this queue is currently 30 
-
+queue_of_events = fetch_items(sensorData)
+# pass queue_of_events into the AI model. 
+# Then, dequeue and then enqueue fetch_item(collection). 
+# rinse and repeat the above two steps throughout the drive. 
+# while loop for polling, can sleep to ensure 3 Hz polling rate from the DB.
 
 # read CSV row from stdin
 csv_row = sys.stdin.read().strip()
