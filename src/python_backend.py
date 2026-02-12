c5ba06ac22ff6e7709177532ca6d471099bc9cdc
SnehDec-TheResurrection <snehasridhar2001@gmail.com>
2026-02-12 12:03:20 -0500

    Added python connection to mongoDB and queue

--- src/python_backend.py
+++ src/python_backend.py
@@ -1,4 +1,24 @@
 import sys
+import os
+from pymongo import MongoClient
+
+def connect_to_DB():
+  mongo_url = os.getenv("MONGO_URL")
+  client = MongoClient(mongo_url)
+  db = client["DrivingBuddy"]
+  collection = db["sensordatas"]
+  return collection
+
+def enqueue(queue, item):
+  queue.append[item]
+
+def dequeue(queue):
+  queue.pop(0)
+
+sensorData = connect_to_DB()
+
+queue_of_events = [] # the size of this queue is currently 30 
+
 
 # read CSV row from stdin
 csv_row = sys.stdin.read().strip()
