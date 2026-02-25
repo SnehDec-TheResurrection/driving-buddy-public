ff3a1e1efc2231378aa627c0740dab4cd4b9a6c1
SnehDec-TheResurrection <snehasridhar2001@gmail.com>
2026-02-25 18:23:45 -0500

    Update sensorDataModel.js

--- src/core/models/sensorDataModel.js
+++ src/core/models/sensorDataModel.js
@@ -12,12 +12,6 @@ const sensorDataSchema = new mongoose.Schema({
   yaw_rate:{type:Number, required:true},
   gps_latitude:{type:String, required:true},
   gps_longitude:{type:String, required:true},
-    
-  // steering_angle_deg: { type: Number },
-  //sharp_turn: { type: Boolean },
-  // lane_deviation: { type: Boolean }, // Uncomment when ready to use
-  inconsistent_speed: { type: Boolean },
-  //hard_braking: { type: Boolean }, 
   lane_offset: {type:Number},
   lane_offset_direction: {type:String},
   trip_ended:{type:Boolean}
