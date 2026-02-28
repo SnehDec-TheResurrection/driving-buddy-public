08a7fa29752c82d283f932e38964c80a78cf7f0a
SnehDec-TheResurrection <snehasridhar2001@gmail.com>
2026-02-28 10:54:24 -0500

    Update server.js

--- src/server.js
+++ src/server.js
@@ -74,19 +74,12 @@ connectMQ();
 
 const wss = new WebSocketServer({ server, path:'/websocky' });
 wss.on('connection', function connection(ws) {
-  let counter = 0 // counts whether a websocket message is being sent for the first time or not.
   ws.on('error', console.error);
   client = ws;
   ws.on('message', function message(data) {
-    if(counter===0){
-     user_id = data;
-     console.log(user_id);
-     ws.send(user_id);
-     counter +=1;
-    }
+     ws.send('recommendation and love letter from Mr. Heroku to Ms. ESP32');
   });
-
-  ws.send('recommendation and love letter from Mr. Heroku to Ms. ESP32');
+  
 });
 
   dashboard_recommendation.on("new_recommendation", (newRecValue) => {
