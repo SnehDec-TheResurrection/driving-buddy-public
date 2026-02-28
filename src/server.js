3eb598c7f2c1d012410535991729857a77f05eee
SnehDec-TheResurrection <snehasridhar2001@gmail.com>
2026-02-28 11:40:34 -0500

    removed lane deviation, removed imu, set yaw rate to degrees instead of radians, subtracted 1 from hour

--- src/server.js
+++ src/server.js
@@ -138,13 +138,15 @@ app.post("/esp32", async (req, res) => {
     const fields = csv_data.split(",");
     //if (fields.length < 7) throw new Error("Invalid CSV");
     
-    let lane_offset = parseFloat(fields[10]);
-    let lane_offset_direction = fields[11];
+    //let lane_offset = parseFloat(fields[10]);
+      let lane_offset = 0;
+      let lane_offset_direction = "centre";
+    //let lane_offset_direction = fields[11];
     
-    if(lane_offset <-1 || lane_offset >1 || lane_offset_direction ==="0"){
-      lane_offset = 0; 
-      lane_offset_direction = "centre";
-    }
+   // if(lane_offset <-1 || lane_offset >1 || lane_offset_direction ==="0"){
+     // lane_offset = 0; 
+     // lane_offset_direction = "centre";
+    //}
     
      const [hours, minutes, seconds] = fields[1].split(":").map(Number);
     const now = new Date();
@@ -155,6 +157,9 @@ app.post("/esp32", async (req, res) => {
         hours,
         minutes,
         seconds);
+      time_with_date.setHours(time_with_date.getHours()-1);
+
+    let yaw_rate = parseFloat(fields[7])*180/Math.PI;    
 
 
     doc = {
@@ -166,7 +171,7 @@ app.post("/esp32", async (req, res) => {
       acceleration_x: parseFloat(fields[4]), 
       acceleration_y: parseFloat(fields[5]), 
       acceleration_z: parseFloat(fields[6]), 
-      yaw_rate: parseFloat(fields[7]),
+      yaw_rate: 0 //yaw_rate,
       gps_latitude: fields[8],
       gps_longitude: fields[9],
       lane_offset,
