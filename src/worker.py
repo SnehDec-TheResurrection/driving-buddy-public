0984031f8a4bedb20df7180f07fd1ef1c4abf409
SnehDec-TheResurrection <snehasridhar2001@gmail.com>
2026-02-28 00:27:02 -0500

    More AI acceleraion_x edits

--- src/worker.py
+++ src/worker.py
@@ -54,7 +54,6 @@ def unscale_joint_preds(y_pred_s, scaler_dyaw, scaler_dv):
 def reconstruct(y_pred_joint_raw, queue_of_events):
     # reconstruct ABS yaw/vel/accel predictions in original units
     yaw_pred_abs = queue_of_events[-1]["yaw_rate"] + y_pred_joint_raw[:, :, IDX_YAW]
-    # vel_abs = vel_last + dv_to_last
     vel_pred_abs = queue_of_events[-1]["speed"] + y_pred_joint_raw[:, :, IDX_VEL]
     return yaw_pred_abs, vel_pred_abs
 
@@ -243,9 +242,13 @@ yaw_predicted, vel_predicted = reconstruct(scaled_predictions_queue, queue_of_ev
 # Create list of dictionaries for AI predictions
 AI_pred_list = []
 for i in range(window_size):
+    # Get velocity of THIS step
+    curr_v = float(vel_predicted[0][i])
+    # Get velocity of PREVIOUS step (or the last real speed if i=0)
+    prev_v = float(vel_predicted[0][i-1]) if i > 0 else queue_of_events[-1]["speed"]
     entry = {
         "speed": float(vel_predicted[0][i]),
-        "acceleration_x": float(scaled_predictions_queue[0][i][IDX_VEL]/0.5),
+        "acceleration_x": float(curr_v-prev_v/0.5),
         "yaw_rate": float(yaw_predicted[0][i]), 
         "acceleration_y": 0.0,
         "lane_offset": 0,
@@ -286,9 +289,14 @@ while trip_ended == False:
     # Create list of dictionaries for AI predictions
     AI_pred_list = []
     for i in range(window_size):
+        # Get velocity of THIS step
+        curr_v = float(vel_predicted[0][i])
+        # Get velocity of PREVIOUS step (or the last real speed if i=0)
+        prev_v = float(vel_predicted[0][i-1]) if i > 0 else queue_of_events[-1]["speed"]
+        
         entry = {
             "speed": float(vel_predicted[0][i]),
-            "acceleration_x": float(scaled_predictions_queue[0][i][IDX_VEL]/0.5),
+            "acceleration_x": float(curr_v-prev_v/0.5),
             "yaw_rate": float(yaw_predicted[0][i]), 
             "acceleration_y": 0.0,
             "lane_offset":0,
