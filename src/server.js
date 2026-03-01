444fd458f208a3b312551292834916032ed504bb
SnehDec-TheResurrection <snehasridhar2001@gmail.com>
2026-02-28 20:18:07 -0500

    Update server.js

--- src/server.js
+++ src/server.js
@@ -138,7 +138,7 @@ app.post("/esp32", async (req, res) => {
       csv_data = "37,00:12:00,1,2,3,4,5,6,8,9, 10, right";
     }
 
-    if (tripID === null){
+    if (current_tripID === null){
         end_trip = "";
       trip_ended = false;
       const now = new Date();
