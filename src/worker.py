0494edfd8b89fafa627d9bbf4c28eff60ad778d3
SnehDec-TheResurrection <snehasridhar2001@gmail.com>
2026-03-02 16:53:28 -0500

    Update worker.py

--- src/worker.py
+++ src/worker.py
@@ -137,6 +137,9 @@ def dequeue(queue):
 def fetch_items(collection, number_of_items):
    global last_timestamp, current_trip_id
    current_timestamp = datetime.now() 
+   first_entry = sensorData.find_one({"tripID": current_trip_id}, sort=[("timestamp", 1)])
+   if first_entry:
+       last_timestamp = first_entry["timestamp"] - timedelta(milliseconds=1)
    while True:
     query = {"tripID": current_trip_id, "timestamp": {"$gt": last_timestamp}}
     print(last_timestamp)
@@ -225,7 +228,7 @@ db=connect_to_DB()
 while True:
 
     trip_ended = False
-    last_timestamp = datetime.now() - timedelta(hours=5)
+    last_timestamp = datetime.now() - timedelta(hours=5) - timedelta(milliseconds=1)
     current_trip_id=0
     inconsistent_speed_instances = []
     sudden_braking_instances = []
