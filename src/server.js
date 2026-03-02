f2ea72b61ce4fa602ad988844e974c3a5d22d405
SnehDec-TheResurrection <snehasridhar2001@gmail.com>
2026-03-01 21:02:52 -0500

    removed lane deviation from dummy end trip doc

--- src/server.js
+++ src/server.js
@@ -135,7 +135,7 @@ app.post("/esp32", async (req, res) => {
     else if (csv_data === "end_of_trip"){
       end_trip ="Thank you for driving!";
       trip_ended = true;
-      csv_data = "37,00:12:00,1,2,3,4,5,6,8,9, 10, right";
+      csv_data = "37,00:12:00,1,2,3,4,5,6,8,9" //, 10, right"; <- add when lane deviation is back. 
     }
 
     if (current_tripID === null){
