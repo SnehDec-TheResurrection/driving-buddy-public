5cd04ced03ac4ee8ffd6fed42a0e9eea4d8caeb9
Maya Shenoy <mayashenoy1218@gmail.com>
2026-03-03 09:50:30 -0500

    Refactor classifier function to remove duplicate checks

--- src/worker.py
+++ src/worker.py
@@ -207,6 +207,12 @@ def squish_into_average(series):
 
 
 def classifier(speed, average_acceleration, acceleration_frequency, yaw_rate, acceleration_y, jerk, lane_deviation_direction,current_average_timestamp, current_average_gps_latitude, current_average_gps_longitude):
+    #Sharp Turning
+    if abs(acceleration_y) > 1 or abs((yaw_rate*3.14/180)*(speed/3.6)) > 1:
+        return f"Be careful before turning.,{current_average_gps_latitude},{current_average_gps_longitude},{current_average_timestamp}"
+     #Sudden Braking
+    if abs(average_acceleration) > 1.0:
+        return f"Start slowing down early.,{current_average_gps_latitude},{current_average_gps_longitude},{current_average_timestamp}" 
     #Inconsistent Acceleration
     if (jerk > 2 and acceleration_frequency < 0.3) or jerk > 3:
         return f"Gradually speed up or slow down early.,{current_average_gps_latitude},{current_average_gps_longitude},{current_average_timestamp}" 
@@ -215,12 +221,7 @@ def classifier(speed, average_acceleration, acceleration_frequency, yaw_rate, ac
         return f"Adjust to the right to stay centred in the lane.,{current_average_gps_latitude},{current_average_gps_longitude},{current_average_timestamp}" 
     elif(lane_deviation_direction == "right"):
         return f"Adjust to the left to stay centred in the lane.,{current_average_gps_latitude},{current_average_gps_longitude},{current_average_timestamp}" 
-    #Sharp Turning
-    if abs(acceleration_y) > 1 or abs((yaw_rate*3.14/180)*(speed/3.6)) > 1:
-        return f"Be careful before turning.,{current_average_gps_latitude},{current_average_gps_longitude},{current_average_timestamp}"
-     #Sudden Braking
-    if abs(average_acceleration) > 1.0:
-        return f"Start slowing down early.,{current_average_gps_latitude},{current_average_gps_longitude},{current_average_timestamp}" 
+    
 
 #Set up the message queue connection
 connection, channel = set_up_mq()
