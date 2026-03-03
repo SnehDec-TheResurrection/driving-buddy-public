460ebe2945475948ea4837513f758d027164cc71
Maya Shenoy <mayashenoy1218@gmail.com>
2026-03-03 00:16:18 -0500

    reorder lat,lon,timestamp in classifier return statements
    timestamp, lat, lon --> lat, lon, timestamp

--- src/worker.py
+++ src/worker.py
@@ -209,18 +209,18 @@ def squish_into_average(series):
 def classifier(speed, average_acceleration, acceleration_frequency, yaw_rate, acceleration_y, jerk, lane_deviation_direction,current_average_timestamp, current_average_gps_latitude, current_average_gps_longitude):
     #Sudden Braking
     if abs(average_acceleration) > 1.0:
-        return f"Start slowing down early.,{current_average_timestamp},{current_average_gps_latitude},{current_average_gps_longitude}" 
+        return f"Start slowing down early.,{current_average_gps_latitude},{current_average_gps_longitude},{current_average_timestamp}" 
     #Sharp Turning
     if abs(acceleration_y) > 1 or abs((yaw_rate*3.14/180)*(speed/3.6)) > 1:
-        return f"Be careful before turning.,{current_average_timestamp},{current_average_gps_latitude},{current_average_gps_longitude}"
+        return f"Be careful before turning.,{current_average_gps_latitude},{current_average_gps_longitude},{current_average_timestamp}"
     #Inconsistent Acceleration
     if (jerk > 2 and acceleration_frequency < 0.3) or jerk > 3:
-        return f"Gradually speed up or slow down early.,{current_average_timestamp},{current_average_gps_latitude},{current_average_gps_longitude}" 
+        return f"Gradually speed up or slow down early.,{current_average_gps_latitude},{current_average_gps_longitude},{current_average_timestamp}" 
     # Lane deviation
     if(lane_deviation_direction == "left"):
-        return f"Adjust to the right to stay centred in the lane.,{current_average_timestamp},{current_average_gps_latitude},{current_average_gps_longitude}" 
+        return f"Adjust to the right to stay centred in the lane.,{current_average_gps_latitude},{current_average_gps_longitude},{current_average_timestamp}" 
     elif(lane_deviation_direction == "right"):
-        return f"Adjust to the left to stay centred in the lane.,{current_average_timestamp},{current_average_gps_latitude},{current_average_gps_longitude}" 
+        return f"Adjust to the left to stay centred in the lane.,{current_average_gps_latitude},{current_average_gps_longitude},{current_average_timestamp}" 
 
 #Set up the message queue connection
 connection, channel = set_up_mq()
