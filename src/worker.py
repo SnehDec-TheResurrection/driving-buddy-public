77af36e310ceca611fac67bd185b6a8bec0fab1a
SnehDec-TheResurrection <snehasridhar2001@gmail.com>
2026-03-02 21:46:18 -0500

    Update worker.py

--- src/worker.py
+++ src/worker.py
@@ -60,21 +60,22 @@ def reconstruct(y_pred_joint_raw, queue_of_events):
     vel_pred_abs = queue_of_events[-1]["speed"] + y_pred_joint_raw[:, :, IDX_VEL]
     return yaw_pred_abs, vel_pred_abs
 
-def cooldown(prediction_text, rec_string):
+def cooldown(prediction_text, rec_string, send_out_duplicate):
     global last_recommendation_time
     current_time = datetime.now()
     time_difference = current_time - last_recommendation_time
 
     # Check if it's the same message AND within the cooldown period
     if prediction_text and prediction_text.split(",")[0] == rec_string and time_difference < COOLDOWN_SECONDS:
-        rec_string = "Duplicate"
+        send_out_duplicate = "Duplicate"
         verdict_true = 0
     elif prediction_text:
         # Update state and trigger recommendation
         rec_string = prediction_text.split(",")[0]
+        send_out_duplicate = prediction_text.split(",")[0]
         last_recommendation_time = current_time
         verdict_true = 1
-    return rec_string, verdict_true
+    return rec_string, verdict_true, send_out_duplicate
         
 def increment_persistent_data(prediction_text, verdict_true):
     global sudden_braking_instances, sharp_turning_instances, inconsistent_speed_instances, lane_deviation_instances
@@ -241,6 +242,8 @@ while True:
     last_recommendation_time = datetime.now()
     dashboard_recommendation_value = ""
     dashboard_recommendation_value_AI = ""
+    send_out_duplicate = ""
+    send_out_duplicate_AI = ""
     
     #Wait for start of trip and get Trip ID
     message_dyno(channel)
@@ -258,11 +261,11 @@ while True:
         # classify real data
         squished_speed, squished_average_acceleration, squished_acceleration_frequency, squished_yaw_rate, squished_acceleration_y, squished_jerk, squished_lane_deviation_direction, squished_timestamp, squished_gps_latitude, squished_gps_longitude=squish_into_average(queue_of_events)
         verdict = classifier(squished_speed, squished_average_acceleration, squished_acceleration_frequency, squished_yaw_rate, squished_acceleration_y, squished_jerk, squished_lane_deviation_direction, squished_timestamp, squished_gps_latitude, squished_gps_longitude)
-        dashboard_recommendation_value, verdict_true = cooldown(verdict, dashboard_recommendation_value)
-        print(dashboard_recommendation_value)
+        dashboard_recommendation_value, verdict_true, send_out_duplicate = cooldown(verdict, dashboard_recommendation_value, send_out_duplicate)
+        print(send_out_duplicate)
         increment_persistent_data(verdict, verdict_true)
         if dashboard_recommendation_value == "Gradually speed up or slow down early." or dashboard_recommendation_value == "Adjust to the right to stay centred in the lane." or dashboard_recommendation_value == "Adjust to the left to stay centred in the lane.":
-            send_message_to_node(channel, dashboard_recommendation_value)
+            send_message_to_node(channel, send_out_duplicate)
         # Convert this into a tensor, X_test, to feed into the AI model.
         data = convert_into_tensor(queue_of_events)
         
@@ -296,10 +299,10 @@ while True:
         # classify AI predicted data and send to the dashboard display 
         squished_AI_speed, squished_AI_average_acceleration, squished_AI_acceleration_frequency, squished_AI_yaw_rate, squished_AI_acceleration_y, squished_AI_jerk, squished_AI_lane_deviation_direction, squished_AI_timestamp, squished_AI_lat, squished_AI_long=squish_into_average(AI_pred_list)
         verdict_AI = classifier(squished_AI_speed, squished_AI_average_acceleration, squished_AI_acceleration_frequency, squished_AI_yaw_rate, squished_AI_acceleration_y, squished_AI_jerk, squished_AI_lane_deviation_direction, squished_AI_timestamp, squished_AI_lat, squished_AI_long)
-        dashboard_recommendation_value_AI = cooldown(verdict_AI, dashboard_recommendation_value_AI)
+        dashboard_recommendation_value_AI, verdict_true, send_out_duplicate_AI = cooldown(verdict_AI, dashboard_recommendation_value_AI, send_out_duplicate_AI)
         if dashboard_recommendation_value_AI == "Start slowing down early." or dashboard_recommendation_value_AI == "Be careful before turning.":
             #send the recommendation via message queue
-            send_message_to_node(channel, dashboard_recommendation_value_AI)
+            send_message_to_node(channel, send_out_duplicate_AI)
     while trip_ended == False:
         next_packets = fetch_items(sensorData, stride)
         if next_packets == -1:
@@ -313,10 +316,10 @@ while True:
         # classify real data
         squished_speed, squished_average_acceleration, squished_acceleration_frequency, squished_yaw_rate, squished_acceleration_y, squished_jerk,squished_lane_deviation_direction, squished_time, squished_lat, squished_long=squish_into_average(queue_of_events)
         verdict = classifier(squished_speed, squished_average_acceleration, squished_acceleration_frequency, squished_yaw_rate, squished_acceleration_y, squished_jerk, squished_lane_deviation_direction, squished_time, squished_lat, squished_long)
-        dashboard_recommendation_value, verdict_true = cooldown(verdict, dashboard_recommendation_value)
-        increment_persistent_data(dashboard_recommendation_value, verdict_true)
+        dashboard_recommendation_value, verdict_true, send_out_duplicate = cooldown(verdict, dashboard_recommendation_value, send_out_duplicate)
+        increment_persistent_data(verdict, verdict_true)
         if dashboard_recommendation_value == "Gradually speed up or slow down early." or dashboard_recommendation_value == "Adjust to the right to stay centred in the lane." or dashboard_recommendation_value == "Adjust to the left to stay centred in the lane.":
-            send_message_to_node(channel, dashboard_recommendation_value)
+            send_message_to_node(channel, send_out_duplicate)
         # Convert this into a tensor, X_test, to feed into the AI model.
         data = convert_into_tensor(queue_of_events)
         data_scaled = loaded_input_scaler.transform(data)
@@ -350,10 +353,10 @@ while True:
         # classify AI predicted data and send to the dashboard display 
         squished_AI_speed, squished_AI_average_acceleration, squished_AI_acceleration_frequency, squished_AI_yaw_rate, squished_AI_acceleration_y, squished_AI_jerk,squished_AI_lane_deviation_direction, squished_AI_time, squished_AI_lat, squished_AI_long=squish_into_average(AI_pred_list)
         verdict_AI = classifier(squished_AI_speed, squished_AI_average_acceleration, squished_AI_acceleration_frequency, squished_AI_yaw_rate, squished_AI_acceleration_y, squished_AI_jerk, squished_AI_lane_deviation_direction, squished_AI_time, squished_AI_lat, squished_AI_long)
-        dashboard_recommendation_value_AI = cooldown(verdict_AI, dashboard_recommendation_value_AI)
+        dashboard_recommendation_value_AI, verdict_true, send_out_duplicate_AI = cooldown(verdict_AI, dashboard_recommendation_value_AI, send_out_duplicate_AI)
         if dashboard_recommendation_value_AI == "Start slowing down early." or dashboard_recommendation_value_AI == "Be careful before turning.":
             #send the recommendation via message queue
-            send_message_to_node(channel, dashboard_recommendation_value_AI)
+            send_message_to_node(channel, send_out_duplicate_AI)
         if queue_of_events[-1]["trip_ended"]==True:
             trip_ended=True
             break
