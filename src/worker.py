f511e2a1ac61d0c8bdcc292d75edcc80460d7633
SnehDec-TheResurrection <snehasridhar2001@gmail.com>
2026-03-09 16:30:31 -0400

    Update worker.py

--- src/worker.py
+++ src/worker.py
@@ -251,7 +251,7 @@ while True:
     dashboard_recommendation_value = ""
     dashboard_recommendation_value_AI = ""
     test_strings = ["Be careful before turning.", "Start slowing down early.", "Gradually speed up or slow down early", "Adjust to the right to stay centred in the lane.", "Adjust to the left to stay centred in the lane."]
-    for i in range(20):
+    while True:
         for test_string in test_strings:
             send_message_to_node(channel, test_string)
             time.sleep(30)
