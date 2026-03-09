cc108594e6bc5c06f5cad81e88ab9a22063a45d5
SnehDec-TheResurrection <snehasridhar2001@gmail.com>
2026-03-09 16:22:56 -0400

    Update worker.py

--- src/worker.py
+++ src/worker.py
@@ -251,7 +251,7 @@ while True:
     dashboard_recommendation_value = ""
     dashboard_recommendation_value_AI = ""
     test_strings = ["Be careful before turning.", "Start slowing down early.", "Gradually speed up or slow down early", "Adjust to the right to stay centred in the lane.", "Adjust to the left to stay centred in the lane."]
-    for test_string in test_string:
+    for test_string in test_strings:
         send_message_to_node(channel, test_string)
         time.sleep(30)
     #Wait for start of trip and get Trip ID
