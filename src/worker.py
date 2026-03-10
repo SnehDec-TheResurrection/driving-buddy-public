67c1120639bf6793297c868e9c2df2a20e5f7252
SnehDec-TheResurrection <snehasridhar2001@gmail.com>
2026-03-10 00:31:42 -0400

    Update worker.py with variable change

--- src/worker.py
+++ src/worker.py
@@ -308,8 +308,8 @@ while True:
         verdict_AI = classifier(squished_AI_speed, squished_AI_average_acceleration, squished_AI_acceleration_frequency, squished_AI_yaw_rate, squished_AI_acceleration_y, squished_AI_jerk, squished_AI_lane_deviation_direction, squished_AI_timestamp, squished_AI_lat, squished_AI_long)
         dashboard_recommendation_value_AI, verdict_true = cooldown(verdict_AI, "AI")
         print(dashboard_recommendation_value_AI)
-        timedelta = datetime.now() - recommendation_lag
-        print("TIme lag: {str(timedelta)}")
+        time_gap_AI = datetime.now() - recommendation_lag
+        print("TIme lag: {str(time_gap_AI)}")
         if dashboard_recommendation_value_AI == "Start slowing down early." or dashboard_recommendation_value_AI== "Be careful before turning.":
             #send the recommendation via message queue
             send_message_to_node(channel, dashboard_recommendation_value_AI)
@@ -371,8 +371,8 @@ while True:
         if dashboard_recommendation_value_AI == "Start slowing down early." or dashboard_recommendation_value_AI == "Be careful before turning.":
             #send the recommendation via message queue
             send_message_to_node(channel, dashboard_recommendation_value_AI)
-        timedelta = datetime.now() - recommendation_lag
-        print("TIme lag: {str(timedelta)}")
+        time_gap_AI = datetime.now() - recommendation_lag
+        print("TIme lag: {str(time_gap_AI)}")
         # classify real data
         squished_speed, squished_average_acceleration, squished_acceleration_frequency, squished_yaw_rate, squished_acceleration_y, squished_jerk,squished_lane_deviation_direction, squished_time, squished_lat, squished_long=squish_into_average(queue_of_events)
         verdict = classifier(squished_speed, squished_average_acceleration, squished_acceleration_frequency, squished_yaw_rate, squished_acceleration_y, squished_jerk, squished_lane_deviation_direction, squished_time, squished_lat, squished_long)
